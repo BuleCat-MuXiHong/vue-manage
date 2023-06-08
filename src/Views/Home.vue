ad4ba53dd843e81bbd2d790e0cf4b4a88df9a8e1
muxihong <17608507695@163.com>
2023-06-09 00:31:51 +0800

    面包屑31开始

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -16,7 +16,7 @@
             </el-card>
             <el-card style="margin-top: 20px; height: 460px;">
                 <el-table
-                    :data="tableData">
+                        :data="tableData">
                     <el-table-column v-for="(val, key) in tableLabel"
                                      :key="key"
                                      :prop="key"
@@ -26,21 +26,26 @@
             </el-card>
         </el-col>
         <el-col :span="16" style="padding-left: 10px">
-            <div class="num" >
+            <div class="num">
                 <el-card v-for="item in countData" :key="item.name" :body-style="{display:'flex',padding:0}">
                     <i class="icon" :class="`el-icon-${item.icon}`" :style="{background:item.color}"></i>
                     <div class="detail">
-                        <p class="price">￥{{item.value}}</p>
-                        <p class="desc">{{item.name}}</p>
+                        <p class="price">￥{{ item.value }}</p>
+                        <p class="desc">{{ item.name }}</p>
                     </div>
                 </el-card>
             </div>
             <el-card style="height: 280px">
-
+                <!--  折线图  -->
+                <div ref="echarts1" style="height: 280px;display: flex;justify-content: center"></div>
             </el-card>
             <div class="graph">
-                <el-card style="height: 260px"></el-card>
-                <el-card style="height: 260px"></el-card>
+                <el-card style="height: 260px">
+                    <div style="height: 260px;display: flex;justify-content: center" ref="echarts2"></div>
+                </el-card>
+                <el-card style="height: 260px">
+                    <div style="height: 200px;display: flex;justify-content: center" ref="echarts3"></div>
+                </el-card>
             </div>
         </el-col>
     </el-row>
@@ -48,13 +53,121 @@
 
 <script>
 import {getData} from "@/api";
+import * as echarts from 'echarts'
 
 export default {
     mounted() {
-        getData().then(({data})=>{
-            const {tableData} = data.data
+        getData().then(({data}) => {
+            const {tableData, userData,videoData} = data.data
             this.tableData = tableData
+            console.log(data)
+
+            // 基于准备好的dom，初始化echarts实例
+            const echarts1 = echarts.init(this.$refs.echarts1)
+            //指定图表的配置和数据
+            let echarts1Option = {}
+            // 处理数据xAxis
+            const {orderData} = data.data
+            const xAxis = Object.keys(orderData.data[0])
+            echarts1Option.xAxis = {
+                data: xAxis
+            }
+            echarts1Option.yAxis = {}
+            echarts1Option.legend = {
+                data: xAxis
+            }
+            echarts1Option.series = []
+            xAxis.forEach(key => {
+                echarts1Option.series.push({
+                    name: key,
+                    data: orderData.data.map(item => item[key]),
+                    type: 'line'
+                })
+            })
+            echarts1.setOption(echarts1Option)
+
+
+            //柱状图
+            const echarts2 = echarts.init(this.$refs.echarts2)
+            let echarts2Option = {
+                legend: {
+                    // 图例文字颜色
+                    textStyle: {
+                        color: "#333",
+                    },
+                },
+                grid: {
+                    left: "20%",
+                },
+                // 提示框
+                tooltip: {
+                    trigger: "axis",
+                },
+                xAxis: {
+                    type: "category", // 类目轴
+                    data: userData.map(item => item.date),
+                    axisLine: {
+                        lineStyle: {
+                            color: "#17b3a3",
+                        },
+                    },
+                    axisLabel: {
+                        interval: 0,
+                        color: "#333",
+                    },
+                },
+                yAxis: [
+                    {
+                        type: "value",
+                        axisLine: {
+                            lineStyle: {
+                                color: "#17b3a3",
+                            },
+                        },
+                    },
+                ],
+                color: ["#2ec7c9", "#b6a2de"],
+                series: [
+                    {
+                        name: '新增用户',
+                        data: userData.map(item => item.new),
+                        type: 'bar'
+                    },
+                    {
+                        name: '活跃用户',
+                        data: userData.map(item => item.active),
+                        type: 'bar'
+                    }
+                ],
+            }
+            echarts2.setOption(echarts2Option)
+
+            //饼状图
+            const echarts3 = echarts.init(this.$refs.echarts3)
+            let echarts3Option = {
+                tooltip: {
+                    trigger: "item",
+                },
+                color: [
+                    "#0f78f4",
+                    "#dd536b",
+                    "#9462e5",
+                    "#a6a6a6",
+                    "#e1bb22",
+                    "#39c362",
+                    "#3ed1cf",
+                ],
+                series: [
+                    {
+                        data:videoData,
+                        type:'pie'
+                    }
+                ],
+            }
+            echarts3.setOption(echarts3Option)
         })
+
+
     },
     data() {
         return {
@@ -102,7 +215,8 @@ export default {
                     icon: "s-goods",
                     color: "#5ab1ef",
                 },
-            ]
+            ],
+
         }
     }
 }
@@ -140,52 +254,61 @@ export default {
     line-height: 28px;
     font-size: 14px;
     color: #999999;
+
     span {
       color: #666666;
       margin-left: 60px;
     }
   }
 }
-.num{
+
+.num {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  .el-card {
+    width: 32%;
+    margin-bottom: 20px;
+  }
+
+  .icon {
+    width: 80px;
+    height: 80px;
+    font-size: 30px;
+    line-height: 80px;
+    text-align: center;
+    color: white;
+  }
+
+  .detail {
     display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    .el-card{
-        width: 32%;
-        margin-bottom: 20px;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 15px;
+
+    .price {
+      font-size: 30px;
+      margin-bottom: 10px;
+      line-height: 30px;
+      height: 30px;
     }
-    .icon{
-        width: 80px;
-        height: 80px;
-        font-size: 30px;
-        line-height: 80px;
-        text-align: center;
-        color: white;
+
+    .desc {
+      font-size: 14px;
+      color: #999999;
+      text-align: center;
     }
-    .detail{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin-left: 15px;
-        .price{
-            font-size: 30px;
-            margin-bottom: 10px;
-            line-height: 30px;
-            height: 30px;
-        }
-        .desc{
-            font-size: 14px;
-            color: #999999;
-            text-align: center;
-        }
-    }
+  }
 }
-.graph{
-    margin-top: 20px;
-    display: flex;
-    justify-content: space-between;
-    .el-card{
-        width: 48%;
-    }
+
+.graph {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+
+  .el-card {
+    width: 48%;
+  }
 }
 </style>
